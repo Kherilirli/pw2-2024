e0d99c1343a0ac662794d76c5c27f86be66b0a30
Kherilirli <januarkherilirli@gmail.com>
2024-02-19 20:47:47 +0700

    tugas1

---
+++ praktikum1/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$nama = "Kherill Irli Januar";
+$jurusan = "Teknik Informatika";
+$semester = 2;
+$sudahlulus = false;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2><?php echo $nama ?></h2>
+    <p>Hai nama saya <?php echo $nama ?></p>
+    <p>Jurusan saya <?php echo $jurusan ?></p>
+    <p>Saat ini saya berada di semester <?php echo $semester ?></p>
+    <p>Saya belum lulus <?php echo $sudahlulus
+    ?></p>
+</body>
+</html>
